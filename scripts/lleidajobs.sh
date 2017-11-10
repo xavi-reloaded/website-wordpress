a262470dc926758dedb437ff34a80e8b9d0aea12
jonathan <jonathancheca@gmail.com>
2017-11-10 09:56:50 +0100

    Modify lleidajobs.sh and translate some json files

--- scripts/lleidajobs.sh
+++ scripts/lleidajobs.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+chmod 777 -R /var/www/html/
 
 wp db reset --yes
 wp core install --url="http://localhost"  --title="LleidaJobs" --admin_user="admin" --admin_password="admin" --admin_email="xavi.hidalgo.fernandez@gmail.com"
+wp core update
+
+wp core language install es_ES
 wp core language activate es_ES
+wp plugin update --all
 
 wp plugin activate wp-job-manager
 wp plugin activate wp-job-manager-companies
@@ -15,6 +20,8 @@ wp plugin activate testimonials-by-woothemes
 wp plugin activate ninja-forms
 wp plugin activate if-menu
 wp plugin activate all-in-one-wp-migration
+
+
 
 #wp theme activate jobify
 #wp ai1wm restore localhost-20171105-052146-763.wpress
